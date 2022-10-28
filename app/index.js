e75cfb0f9ff95348cfc1fcd85732d55ff0c35e18
Nic <nic.dan.costea@gmail.com>
2022-10-28 13:44:22 +0200

    checkpoint - "-" & "+" buttons action
    - increase or decrease the nr of possible teams based on the total number of members

--- app/index.js
+++ app/index.js
@@ -1,9 +1,18 @@
 console.log("Testingggg!");
 
-let totalNames = null; // counts each time a name is appended to the display div
+let totalNames = 0; // counts each time a name is appended to the display div
 const totalNamesArray = []; // contains the names that are appended to the display div
 const inputErrorPlaceholder = "! Please input a name that contains at least 3 characters";
 const inputValidPlaceholder = "enter name";
+const teamErrorMessage = "Please add a member";
+
+const nameInput = document.querySelector("#inputName #name");
+const addBtn = document.getElementById("addBtn");
+const displayInputName = document.getElementById("displayInputtedNames");
+
+const minusBtn = document.getElementById("minusBtn");
+const plusBtn = document.getElementById("plusBtn");
+const nrOfTeams = document.getElementById("nrOfTemasDisplay");
 
 /* 
                                                   STEP 1
@@ -13,10 +22,6 @@ clicking the "add" button, display it in the
 "displayInputtedNames" div [and remove the default text]
 --------------------------------------------------------
 */
-const nameInput = document.querySelector("#inputName #name");
-const addBtn = document.getElementById("addBtn");
-const displayInputName = document.getElementById("displayInputtedNames");
-
 const inputValue = function () {
   // create the new div, added a syling class, attributed the input value
   const inputValue = nameInput.value;
@@ -27,6 +32,7 @@ const inputValue = function () {
     nameInput.placeholder = inputErrorPlaceholder;
   } else {
     nameInput.placeholder = inputValidPlaceholder;
+
     const displayDiv = document.createElement("div");
 
     displayDiv.classList.add("displayNameInDiv");
@@ -47,3 +53,62 @@ const inputValue = function () {
 };
 
 addBtn.addEventListener("click", inputValue);
+
+/* 
+                                                  STEP 2
+--------------------------------------------------------
+upon clicking the - and + buttons, the value [that is 
+by default 0] will increase / decrease accordingly by 1;
+
+the buttons will work only depending on the totalNames
+value [each team must have 1 member, but the default
+number of team mates is 2 ---> so for 3 names there
+will be 2 teams: one with the default number of 
+members <2>
+, and the other team that only has 1 member]
+--------------------------------------------------------
+*/
+
+const createTeam = function () {
+  let nr = totalNames;
+  let totalPossibleTeams = Math.round(nr / 2);
+  if (totalPossibleTeams === 0) {
+    nrOfTeams.classList.add("teamErrorMessage");
+    nrOfTeams.innerText = teamErrorMessage;
+  } else {
+    nrOfTeams.classList.remove("teamErrorMessage");
+
+    nrOfTeams.innerText = `${totalPossibleTeams}`;
+  }
+};
+plusBtn.addEventListener("click", createTeam);
+
+const removeTeam = function () {
+  let nr = totalNames;
+  let totalPossibleTeams = Math.floor(nr / 2);
+  if (totalPossibleTeams === 0) {
+    nrOfTeams.classList.add("teamErrorMessage");
+    nrOfTeams.innerText = teamErrorMessage;
+  } else {
+    nrOfTeams.classList.remove("teamErrorMessage");
+
+    nrOfTeams.innerText = `${totalPossibleTeams}`;
+  }
+};
+minusBtn.addEventListener("click", removeTeam);
+
+/* 
+                                                  STEP 3
+--------------------------------------------------------
+upon clicking the - and + buttons, the value [that is 
+by default 0] will increase / decrease accordingly by 1;
+
+the buttons will work only depending on the totalNames
+value [each team must have 1 member, but the default
+number of team mates is 2 ---> so for 3 names there
+will be 2 teams: one with the default number of 
+members <2>
+, and the other team that only has 1 member]
+--------------------------------------------------------
+*/
+const bigDisplayDiv = document.querySelector(".namesAndDisplays");
